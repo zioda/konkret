42bd266ffb65a9acb77e4f5999299f8f84624c80
Kamil Błaszczyk <zioda1992@gmail.com>
2015-05-25 19:20:01 +0200

    1sze commit concrete5

---
+++ concrete5.7.4.2/application/config/generated_overrides/concrete.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------
+ * Generated 2015-05-25T19:17:40+02:00
+ *
+ * @item      misc.do_page_reindex_check
+ * @group     concrete
+ * @namespace null
+ * -----------------------------------------------------------------------------
+ */
+return array(
+    'locale' => 'pl_PL',
+    'site' => 'Kamilo Photos',
+    'version_installed' => '5.7.4.2',
+    'misc' => array(
+        'access_entity_updated' => 1432572772,
+        'do_page_reindex_check' => false
+    )
+);
---
+++ concrete5.7.4.2/application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/377338abea5b1940/5cca4c0032cdd59e.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_3eefb90e34492ee3a1f9eacabaec6898_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1432983999;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1432572812;
